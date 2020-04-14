7b74a3a6a3522fcf2ba5e4c74d3aa1401ae9df91
igadiel <igadiel@liveperson.com>
2020-04-14 14:59:12 +0300

    Initial commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,14 @@
+import {Controller, Get} from '@nestjs/common';
+import {TasksService} from "./tasks.service";
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private taskService: TasksService){}
+
+    @Get()
+    getAllTasks() {
+        debugger;
+        return this.taskService.getAllTasks()
+    }
+
+}
